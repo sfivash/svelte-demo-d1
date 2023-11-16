25a831fa0df7338b1eaba2c2adf8bc7d98f06dd0
Stephen Fivash <stephen.fivash@gmail.com>
2023-11-15 22:31:56 -0800

    Fixed db name

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -3,7 +3,7 @@ import type { D1Database } from "@cloudflare/workers-types";
 import type { RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async function ({ request, platform }) {
-  const db = binding<D1Database>("SHOWDD23_DB", { fallback: platform?.env! });
+  const db = binding<D1Database>("DB", { fallback: platform?.env! });
   let result = await db.prepare("SELECT * FROM users LIMIT 5").run();
   return new Response(JSON.stringify(result));
 };

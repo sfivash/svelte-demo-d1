d7956b473a10c4a5029a3f0099ed0856458e7bce
Stephen Fivash <stephen.fivash@gmail.com>
2023-11-15 22:29:56 -0800

    Added cf-bindings-proxy

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -1,8 +1,9 @@
+import { binding } from "cf-bindings-proxy";
+import type { D1Database } from "@cloudflare/workers-types";
 import type { RequestHandler } from "@sveltejs/kit";
 
-export const GET: RequestHandler = async function({ request, platform }) {
-  let result = await platform.env.DB.prepare(
-    "SELECT * FROM users LIMIT 5"
-  ).run();
+export const GET: RequestHandler = async function ({ request, platform }) {
+  const db = binding<D1Database>("SHOWDD23_DB", { fallback: platform?.env! });
+  let result = await db.prepare("SELECT * FROM users LIMIT 5").run();
   return new Response(JSON.stringify(result));
-}
+};
